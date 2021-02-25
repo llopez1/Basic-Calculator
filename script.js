fedda3ab9339d3fead4dbadaa30f6d30bd71b0c2
abbysudario <abysudario@gmail.com>
2021-02-24 23:08:25 -0800

    starter files upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//variable declaration
+
+
+//addEventListener
+
+
+//functions
+
+
+//dark mode
+document.body.onkeyup = function(e){
+  if(e.key === 'd') {
+    document.body.classList.toggle('dark');
+    
+  }
+}
